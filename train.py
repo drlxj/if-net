0148419899ca468aed767316b7284d03ae502878
zengpy17 <zengyuqi0307@126.com>
2022-04-25 04:50:12 +0200

    [modified] Fix a bug when saving checkpoint in parallel manner

--- train.py
+++ train.py
@@ -67,7 +67,7 @@ def train_basic(rank, exp_name, world_size, args, train_index_total, val_index_t
     val_dataset = voxelized_data.VoxelizedDataset('val', voxelized_pointcloud= args.pointcloud , pointcloud_samples= args.pc_samples, res=args.res, sample_distribution=args.sample_distribution,
                                            sample_sigmas=args.sample_sigmas ,num_sample_points=50000, batch_size=args.batch_size, num_workers=0, world_size = world_size, rank = rank, partition_index = val_index)   
 
-    trainer = training.Trainer(ddp_model, ddp_model.device, train_dataset, val_dataset,exp_name, rank = rank, world_size = world_size, optimizer=args.optimizer)
+    trainer = training.Trainer(ddp_model, ddp_model.device, train_dataset, val_dataset,exp_name, rank = rank, world_size = world_size, optimizer=args.optimizer, parallel=True)
     trainer.train_model(1500)
 
     cleanup()

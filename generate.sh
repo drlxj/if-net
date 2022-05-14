d92e21663db0a4be0b0db3bbe7e42e225be4dbb4
Xuejing Luo <xueluo@eu-login-44.euler.ethz.ch>
2022-05-14 16:34:16 +0200

    [modified] Change generate.sh

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
-python generate.py -std_dev 0.1 0.01 -res 32 -m ShapeNet32Vox -checkpoint 10 -batch_points 400000
-python generate.py -std_dev 0.1 0.01 -res 32 -m ShapeNet128Vox -checkpoint 17 -batch_points 400000
-python generate.py -std_dev 0.1 0.01 -res 32 -m SVR -checkpoint 6 -batch_points 400000
+python generate.py -std_dev 0.1 0.01 -res 32 -m ShapeNet32Vox -checkpoint 6 -batch_points 400000
+#python generate.py -std_dev 0.1 0.01 -res 32 -m ShapeNet128Vox -checkpoint 17 -batch_points 400000
+#python generate.py -std_dev 0.1 0.01 -res 32 -m SVR -checkpoint 6 -batch_points 400000
 
 
